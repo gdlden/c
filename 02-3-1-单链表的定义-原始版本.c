7b319f6be86ae33f8d85db235e2c2b4492561146
hukss <ghyilcr@163.com>
2024-02-17 23:35:40 +0800

    不带头节点单链表的初始化

---
+++ 02-3-1-单链表的定义-原始版本.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//定义结构体积，作为链表的每一个节点的数据结构
+struct LNode
+{
+    //数据域，保存该节点的数据
+    int data;
+    //指针域，保存了下一个节点的地址
+    struct LNode* next;
+};
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
